9949c4b176558a08b416c7765b7752750421c9b2
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-05-29 15:07:52 -0300

    Create, read and read with query string of the games route

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ dotenv.config()
 app.get('/categories', async (req, res) => {
 	try {
 		const categories = await connection.query(`
-			SELECT * FROM categories
+			SELECT 
+				* 
+			FROM 
+				categories
 		`)
 		res.status(200).send(categories.rows)
 	} catch (err) {
@@ -22,32 +25,137 @@ app.get('/categories', async (req, res) => {
 	}
 })
 
-
 app.post('/categories', async (req, res) => {
 	const category = req.body.name
-	console.log(category)
 	const categorySchema = joi.object({
 		name: joi.string()
 			.required()
 	})
-	const validationEntry = categorySchema.validate({name: category})
-	if(validationEntry.error){
+	const validation = categorySchema.validate({name: category})
+	if(validation.error){
 		res.sendStatus(400)
         return;
     }
 	try {
-		const verificationOfExistence = await connection.query(`
-			SELECT * FROM categories WHERE name LIKE ($1)
+		const categoryValidation = await connection.query(`
+			SELECT 
+				* 
+			FROM 
+				categories 
+			WHERE 
+				name LIKE ($1)
 		`, [category])
-		console.log(verificationOfExistence)
-		if (verificationOfExistence.rowCount != 0) {
+		if (categoryValidation.rowCount != 0) {
 			return res.sendStatus(409)
 		}
 		await connection.query(`
-			INSERT INTO categories (name) VALUES ($1)
+			INSERT INTO 
+				categories (name) 
+			VALUES 
+				($1)
 		`, [category])
 		res.status(201).send('Entrada salva')
 	} catch (err) {
+		res.sendStatus(500)
+	}
+})
+
+app.get('/games', async (req, res) => {
+	const name = req.query.name
+	if(name != undefined){
+		try {
+			await connection.query(`
+				SELECT 
+					games.*, categories.name as categoryName 
+				FROM 
+					games 
+				JOIN 
+					categories 
+				ON 
+					games."categoryId" = categories.id
+			`)
+			const filteredGames = await connection.query(`
+				SELECT 
+					* 
+				FROM 
+					games 
+				WHERE 
+					upper(name) LIKE upper($1);
+		`, [(`%${name}%`)])
+			res.status(200).send(filteredGames.rows)
+		} catch (err) {
+			res.sendStatus(500)
+		}
+	} else {
+		try {
+			const games = await connection.query(`
+				SELECT 
+					games.*, categories.name as categoryName 
+				FROM 
+					games 
+				JOIN 
+					categories 
+				ON 
+					games."categoryId" = categories.id
+			`)
+			res.status(200).send(games.rows)
+		} catch (err) {
+			res.sendStatus(500)
+		}
+	}
+})
+
+app.post('/games', async (req, res) => {
+	const { name, image, stockTotal, categoryId, pricePerDay } = req.body
+	const gameSchema = joi.object({
+		name: joi.string()
+			.required(),
+		image: joi.string()
+			.uri()
+			.required(),
+		stockTotal: joi.number()
+			.integer()
+			.min(1),
+		pricePerDay: joi.number()
+			.integer()
+			.min(1)
+	})
+	const validation = gameSchema.validate({name, image, stockTotal, pricePerDay})
+	if(validation.error){
+		res.sendStatus(400)
+        return;
+    }
+	try {
+		const idValidation = await connection.query(`
+			SELECT 
+				* 
+			FROM 
+				categories 
+			WHERE 
+				id = ($1) 
+		`, [categoryId])
+		if (idValidation.rowCount == 0) {
+			return res.sendStatus(400)
+		}
+		const gameValidation = await connection.query(`
+			SELECT 
+				* 
+			FROM 
+				games 
+			WHERE 
+				name = ($1) 
+		`, [name])
+		if (gameValidation.rowCount != 0) {
+			return res.sendStatus(409)
+		}
+		await connection.query(`
+			INSERT INTO
+				games (name, image, "stockTotal", "categoryId", "pricePerDay")
+			VALUES 
+				($1, $2, $3, $4, $5)
+		`, [name, image, stockTotal, categoryId, pricePerDay])
+		res.status(201).send('Entrada salva')
+	} catch(err) {
 		res.sendStatus(500)
 	}
 })
